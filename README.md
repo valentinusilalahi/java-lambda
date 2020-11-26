6d2e6891220e7bbdab7b5fef4df9eb6b8f580a6e
Valentinus Silalahi <valentinus@doku.com>
2020-11-26 21:12:50 +0700

    1-implementasi anonymous-class java 7 kebawah

---
+++ README.md
@@ -0,0 +1,11 @@
+### Lambda Expression
+```fungsi yang tidak memiliki nama```
+* di java lambda adalah shortcut untuk anonymous class (java 8 ke atas)
+* mengimplementasikan anonymous class yang memiliki satu method
+
+* Consumer
+* Function : lambda memiliki parameter dan memiliki value
+* Predicate : lambda menerima 1 param dan mengembalikan boolean value
+* Suplier : tidak memiliki param tapi mengembalikan value
+
+* BeFunction : 2 param 1 return value
--- src/main/java/com/silalahi/valentinus/LambdaApp.java
+++ src/main/java/com/silalahi/valentinus/LambdaApp.java
@@ -5,16 +5,11 @@ import java.util.function.Predicate;
 
 public class LambdaApp {
     public static void main(String[] args) {
-
-//        Predicate<String> predicate = new Predicate<String>() {
-//            @Override
-//            public boolean test(String s) {
-//                return s != null;
-//            }
-//        };
-
-        Predicate<String> predicate = (String value) -> {
-            return value != null;
+        Predicate<String> predicate = new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s != null;
+            }
         };
 
         Function<String, String> function = new Function<String, String>() {
@@ -24,5 +19,7 @@ public class LambdaApp {
             }
         };
 
+        System.out.println(predicate);
+        System.out.println(function);
     }
 }
