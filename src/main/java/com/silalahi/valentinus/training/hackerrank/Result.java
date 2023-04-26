74627931131910ae69c1bbef680301d20b1db160
Valentinus Silalahi <valentinus@doku.com>
2023-04-26 15:50:47 +0700

    basic hackerrank find median and flitting matrix

---
+++ src/main/java/com/silalahi/valentinus/training/hackerrank/Result.java
@@ -0,0 +1,59 @@
+package com.silalahi.valentinus.training.median;
+
+
+import java.util.Collections;
+import java.util.List;
+
+class Result {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+        // Write your code here
+        // Sort the array in ascending order
+        Collections.sort(arr);
+
+        // Find the middle index
+        int midIndex = arr.size() / 2;
+
+        // If the array has an odd length, return the middle element
+        if (arr.size() % 2 == 1) {
+            return arr.get(midIndex);
+        }
+
+        // If the array has an even length, return the average of the middle two elements
+        int midElement1 = arr.get(midIndex - 1);
+        int midElement2 = arr.get(midIndex);
+        return (midElement1 + midElement2) / 2;
+    }
+
+    /*
+     * Complete the 'flippingMatrix' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+     */
+
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+        // Write your code here
+        int n = matrix.size();
+        int sum = 0;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int topLeft = matrix.get(i).get(j);
+                int topRight = matrix.get(i).get(n - 1 - j);
+                int bottomLeft = matrix.get(n - 1 - i).get(j);
+                int bottomRight = matrix.get(n - 1 - i).get(n - 1 - j);
+                sum += Math.max(Math.max(topLeft, topRight), Math.max(bottomLeft, bottomRight));
+            }
+        }
+        return sum;
+
+    }
+
+}
---
+++ src/main/java/com/silalahi/valentinus/training/hackerrank/Solution.java
@@ -0,0 +1,72 @@
+package com.silalahi.valentinus.training.median;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    /*
+    * main for median
+    */
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                .map(Integer::parseInt)
+//                .collect(toList());
+//
+//        int result = Result.findMedian(arr);
+//
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+//    }
+
+
+    /*
+     * main for fletting the matrix
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<List<Integer>> matrix = new ArrayList<>();
+
+                IntStream.range(0, 2 * n).forEach(i -> {
+                    try {
+                        matrix.add(
+                                Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                        .map(Integer::parseInt)
+                                        .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                int result = Result.flippingMatrix(matrix);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
